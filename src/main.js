1c380a917fe9bedac1d6de69567b7e3ea4285295
SummerA213 <1287884194@qq.com>
2021-10-18 17:37:49 +0800

    perform

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store";
 import {Toast} from "vant"
+import VueLazyload from "vue-lazyload";
 
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue ()
 //安装插件
+//弹窗
 Vue.use(Toast)
+//懒加载插件
+Vue.use(VueLazyload,{
+  loading:require('assets/img/common/z.png')
+})
 new Vue({
   render: h => h(App),
   router,
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,59 @@
 <template>
+  <div id="category">
+    <nav-bar class="nav-bar"><div slot="center">商品分类</div></nav-bar>
+    <div class="content">
+      <scroll id="tab-content" ref="scroll">
+        <div>
+          <tab-content-category/>
+          <tab-control :titles="['综合', '新品', '销量']"/>
+          <goods-list/>
+        </div>
+      </scroll>
+    </div>
+  </div>
 </template>
 
 <script>
-  import BScroll from 'better-scroll'
+  import NavBar from 'components/common/navbar/NavBar'
+
+  import TabControl from 'components/content/tabControl/TabControl'
+  import Scroll from 'components/common/scroll/Scroll'
+  import GoodsList from 'components/content/goods/GoodsList'
 
   export default {
-    name: "Category"
+    name: "Category",
+    components: {
+      NavBar,
+      TabControl,
+      Scroll,
+      GoodsList
+    }
   }
 </script>
 
 <style scoped>
+  #category {
+    height: 100vh;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+
+  .content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 44px;
+    bottom: 49px;
+
+    display: flex;
+  }
+
+  #tab-content {
+    height: 100%;
+    flex: 1;
+  }
 </style>
